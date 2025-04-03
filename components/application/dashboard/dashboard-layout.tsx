ea6fdb4118870c65df2cfbd4c0bbb5423b45632b
YLudo <147008535+YLudo@users.noreply.github.com>
2025-04-03 14:11:17 +0200

    feat: add dashboard header

--- components/application/dashboard/dashboard-layout.tsx
+++ components/application/dashboard/dashboard-layout.tsx
@@ -3,10 +3,13 @@ import NextTravelCard from "./next-travel-card";
 
 const DashboardLayout = () => {
     return (
-        <section className="grid lg:grid-cols-3 mt-4 gap-4">
-            <div className="space-y-4">
-                <NextTravelCard />
-                <InvitationsCard />
+        <section className="mt-4">
+            <h1 className="font-bold text-xl">Tableau de bord</h1>
+            <div className="grid lg:grid-cols-3 mt-4 gap-4">
+                <div className="space-y-4">
+                    <NextTravelCard />
+                    <InvitationsCard />
+                </div>
             </div>
         </section>
     )
